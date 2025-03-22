155b4481df7df1cdb0f086439666e9e9cf2f3349
abassadebayo <greatmoh007@gmail.com>
2025-03-22 13:28:52 +0100

    Repository Interfaces

--- src/pcms-api/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/pcms-api/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312fb0fa946b38dd7f34e04b5bc25127ee23ccc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
