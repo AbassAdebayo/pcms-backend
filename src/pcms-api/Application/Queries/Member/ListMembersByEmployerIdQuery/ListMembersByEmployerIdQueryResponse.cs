e9f07051f1fd1c1ec1ba94d0d507cb2310a536c5
abassadebayo <greatmoh007@gmail.com>
2025-03-23 15:42:04 +0100

    List members by employer id query

---
+++ src/pcms-api/Application/Queries/Member/ListMembersByEmployerIdQuery/ListMembersByEmployerIdQueryResponse.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.Member.ListMembersByEmployerIdQuery
+{
+    public sealed record ListMembersByEmployerIdQueryResponse(PaginatedResult<Domain.Entities.Member> Members)
+    {
+    }
+}
