f5254601692726360a910a7f2f24b4ec55d2f9ae
abassadebayo <greatmoh007@gmail.com>
2025-03-24 10:33:45 +0100

    create member endpoint

---
+++ src/pcms-api/Api/Controllers/MemberController/MemberController.cs
@@ -0,0 +1,36 @@
+﻿using Api.Filters;
+using Application.Commands.Employer;
+using Application.Commands.Member.CreateMemberCommand;
+using Application.Models;
+using Application.Queries.Employer;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Controllers.MemberController
+{
+    [Route("api/member")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<MemberController> _logger;
+
+        public MemberController(IMediator mediator, ILogger<MemberController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost("create")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ValidationResultModel))]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(BaseResponse))]
+        public async Task<IActionResult> Create([FromBody] CreateMemberCommand request)
+        {
+            var response = await _mediator.Send(request);
+            return response.Succeeded ? Ok(response) : BadRequest(response);
+        }
+
+    }
+}
