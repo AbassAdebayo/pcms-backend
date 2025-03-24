2f416aa70009fffcf8e3050a3ae5b07c53be8e57
abassadebayo <greatmoh007@gmail.com>
2025-03-24 10:38:14 +0100

    Get member by id endpoint

--- src/pcms-api/Api/Controllers/MemberController/MemberController.cs
+++ src/pcms-api/Api/Controllers/MemberController/MemberController.cs
@@ -3,6 +3,7 @@ using Application.Commands.Employer;
 using Application.Commands.Member.CreateMemberCommand;
 using Application.Models;
 using Application.Queries.Employer;
+using Application.Queries.Member.GetMemberByIdQuery;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -32,5 +33,16 @@ namespace Api.Controllers.MemberController
             return response.Succeeded ? Ok(response) : BadRequest(response);
         }
 
+
+        [HttpGet("id/{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ValidationResultModel))]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(BaseResponse))]
+        public async Task<IActionResult> GetById([FromRoute] Guid Id)
+        {
+            var request = new GetMemberByIdQuery(Id);
+            var response = await _mediator.Send(request);
+            return response.Succeeded ? Ok(response) : BadRequest(response);
+        }
     }
 }
